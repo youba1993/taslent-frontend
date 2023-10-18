108ba5dfe32930d9dd2632782195979bc765d97d
youba idjis <youba.etud@gmail.com>
2023-10-18 01:44:37 -0700

    mission

---
+++ src/components/MissionSection/Mission.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './mission.css'
+
+function Mission() {
+    return (
+        <div className='ourMission'>
+            <h1>notre mission</h1>
+            <div className='mission-text'>
+                <p>
+                    à Taslent, nous nous engageons à bâtir une communauté forte et inclusive où chacun se sent accueilli et valorisé. Notre mission est de :
+                </p>
+                <ul>
+                    <li>Promouvoir un sentiment d’appartenance et de convivialité parmi les membres de la communauté.</li>
+                    <li>Favoriser un environnement de respect, d’empathie et de compréhension.</li>
+                    <li>Encourage la collaboration et la coopération vers des objectifs partagés.</li>
+                    <li>Célébrer la diversité et promouvoir les échanges culturels.</li>
+                    <li>Soutenir le bien-être et le développement de tous les membres de la communauté.</li>
+                </ul>
+                <p>
+                    Nous pensons qu’en travaillant ensemble, nous pouvons créer un avenir meilleur pour nous-mêmes et pour les générations futures.
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Mission;
+
+
+
+
+
+
+
--- src/pages/Village.js
+++ src/pages/Village.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection/HeroSection'
+import Mission from '../components/MissionSection/Mission'
 
 const  Village = () => {
   return (
     <>
     <HeroSection page="Village" cName="hero-mid" />
+    <Mission />
     </>
   )
 }
