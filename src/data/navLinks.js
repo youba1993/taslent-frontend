54b228e675e4ca130dbc5443c35350454f8d5e68
youba idjis <youba.etud@gmail.com>
2023-10-19 16:15:15 -0700

    acctualite

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -1,3 +1,5 @@
+
+
 export const links = [
     {
         title: 'Accueil',
@@ -82,3 +84,109 @@ export const hero = [
         imgUrl:"https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/river1_BTIMtc7i4.jpg?updatedAt=1679708130117",
     }
 ];
+
+export const acctLinks = [
+    {
+        title: 'Magazine',
+        url: '/magazine',
+    },
+    {
+        title: 'Culture',
+        url: '/culture',
+    },
+    {
+        title: 'Sports',
+        url: '/sports',
+    },
+    {
+        title: 'Environment',
+        url: '/environment',
+    },
+]
+
+export const galimages = [
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/carroussel4_op0MopeC-?updatedAt=1679202993528",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/carroussel4_op0MopeC-?updatedAt=1679202993528",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/river_tpIblbUVx?updatedAt=1679203922343",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/river_tpIblbUVx?updatedAt=1679203922343",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/carrousel6_IfcGAY-Yr?updatedAt=1679203036366",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/carrousel6_IfcGAY-Yr?updatedAt=1679203036366",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Carrousel2_VCB1aYmw0?updatedAt=1679112586801",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Carrousel2_VCB1aYmw0?updatedAt=1679112586801",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/culture4_E4VykRfsw.jpg?updatedAt=1697697974491",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/culture4_E4VykRfsw.jpg?updatedAt=1697697974491",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/le_matin_grande_Q3wIv6TDQc.jpg?updatedAt=1697704338172",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/le_matin_grande_Q3wIv6TDQc.jpg?updatedAt=1697704338172",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/la_depeche_grande2_rM6Gt6HqX.jpg?updatedAt=1697704394778",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/la_depeche_grande2_rM6Gt6HqX.jpg?updatedAt=1697704394778",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/clubfoot2_F5Sklsfex.jpg?updatedAt=1697701111856",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/clubfoot2_F5Sklsfex.jpg?updatedAt=1697701111856",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/movein_big_0U4_s5XiPq.jpg?updatedAt=1697704386957",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/movein_big_0U4_s5XiPq.jpg?updatedAt=1697704386957",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/jst2_xmlk8i7sO0.jpg?updatedAt=1697701112425",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Clubs/jst2_xmlk8i7sO0.jpg?updatedAt=1697701112425",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Traditions/iwajivan%206_EIi5Ymjyt.jpg?updatedAt=1697690022946",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Traditions/iwajivan%206_EIi5Ymjyt.jpg?updatedAt=1697690022946",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Traditions/iwajivan2_uDttw0hdS.jpg?updatedAt=1697690021624",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/Traditions/iwajivan2_uDttw0hdS.jpg?updatedAt=1697690021624",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/assemble_QpsGTTnbV.jpg?updatedAt=1697526494595",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/assemble_QpsGTTnbV.jpg?updatedAt=1697526494595",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Projets/extension-centre_zAJFE5b88.jpg?updatedAt=1697668958478",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Projets/extension-centre_zAJFE5b88.jpg?updatedAt=1697668958478",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/assemble-4_KZ6fKzNLl.jpg?updatedAt=1697664663668",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/assemble-4_KZ6fKzNLl.jpg?updatedAt=1697664663668",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/(assembly1970)adjournment_big_N2xOahHmni.gif?updatedAt=1697659982422",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/(assembly1970)adjournment_big_N2xOahHmni.gif?updatedAt=1697659982422",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/(assembly1970)dues_big_2B6WCWrz9J.gif?updatedAt=1697659980959",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/(assembly1970)dues_big_2B6WCWrz9J.gif?updatedAt=1697659980959",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/(assembly2000)session1_big_OaUxMwAoj.gif?updatedAt=1697659980340",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/(assembly1970)walking_big_aXvmulJ3t.gif?updatedAt=1697659979728",
+    },
+    {
+      original: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/(assembly1970)session_big_LeUDJ9neG.gif?updatedAt=1697659979000",
+      thumbnail: "https://ik.imagekit.io/nvnw6o7ew/Taslent/projects/Assembly/(assembly1970)session_big_LeUDJ9neG.gif?updatedAt=1697659979000",
+    },
+    
+  ];
+
+  export const socialMediaLinks = [
+    { icon: 'fa-brands fa-facebook-square', url: 'https://www.facebook.com/profile.php?id=100063650622647' ,name: "Taslent Culture et Environnement" },
+    { icon: 'fa-brands fa-facebook-square', url: 'https://www.facebook.com/groups/6259572290726554', name: "Comité des Notables Tadarth TASLENT" },
+    { icon: 'fa-brands fa-facebook-square', url: 'https://www.facebook.com/profile.php?id=100089060176531', name: "L'association Culturelle des Femmes TAFATH TASLENT" },
+    { icon: "fa-solid fa-globe", url: 'https://www.taslent.com', name: "Taslent.com " },  
+  ];
--- src/pages/Acctualite.js
+++ src/pages/Acctualite.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection/HeroSection'
+import ImageGallery from "react-image-gallery";
+import { galimages, socialMediaLinks } from "../data/navLinks"
+
 
 const Acctualite = () => {
   return (
     <>
     <HeroSection page="Acctualite" cName="hero-mid"/>
+    <div className='acctualite'>
+    <h2>Pour découvrir nos dernières actualités, consultez les liens situés en bas de cette page. Ils vous redirigeront vers nos comptes sur les réseaux sociaux.</h2>
+    
+    <ImageGallery items={galimages} loading thumbnailLoading/>
+
+    <ul className="social-links-list">
+      {socialMediaLinks.map((link, index) => (
+        <li key={index} className="social-link-item">
+          <a href={link.url} target="_blank" rel="noopener noreferrer">
+          <i class={link.icon}>{link.name}</i>
+          </a>
+        </li>
+      ))}
+    </ul>
+
+    </div>
+    
     </>
   )
 }
 
 export default Acctualite
+
+
+
