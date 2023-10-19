c40d2f056ef2825dd89b1e18c17a71d7c7729ee8
youba idjis <youba.etud@gmail.com>
2023-10-18 18:51:17 -0700

    Contacts form netlify

--- src/components/ContactsFile/ContactsForm.js
+++ src/components/ContactsFile/ContactsForm.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contactsform.css';
 
 const ContactsForm = () => {
+
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [tel, setTel] = useState("");
+  const [subject, setSubject] = useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", nom, prenom, email,tel,subject, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
   return (
-    <div className='form-container'>
-        <h1>Envoyer nous vos messages</h1>
-        <form name="contact" method="POST" data-netlify="true">
-            <input required type='text' name='Nom' placeholder='Nom'/>
-            <input required type='text' name='prenom' placeholder='Prenom' />
-            <input required type='email' name='email' placeholder='Email' />
-            <input required type='tel' name='tel' placeholder='Numéro de téléphone' />
-            <input required type='text' name='sujet' placeholder='Sujet' />
-            <textarea name='message' placeholder='Ecrivez votre message ici..' rows={4}></textarea>
-            <button type="submit">Envoyez</button>
-        </form>
+    <div id="contact" className='form-container'>
+      <h1>Envoyer nous vos messages</h1>
+      <input type="hidden" name="form-name" value="contact" />
+      <form name="contact" method="POST" data-netlify="true">
+        <input required type='text' name='Nom' id='Nom' placeholder='Nom' onChange={(e) => setNom(e.target.value)}/>
+        <input required type='text' name='prenom' id="prenom" placeholder='Prenom' onChange={(e) => setPrenom(e.target.value)}/>
+        <input required type='email' name='email' is="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
+        <input required type='tel' name='tel' id="tel" placeholder='Numéro de téléphone' onChange={(e) => setTel(e.target.value)}/>
+        <input required type='text' name='sujet' id="sujet" placeholder='Sujet' onChange={(e) => setSubject(e.target.value)}/>
+        <textarea name='message' id="message" placeholder='Ecrivez votre message ici..' rows={4} onChange={(e) => setMessage(e.target.value)}></textarea>
+        <button type="submit">Envoyez</button>
+      </form>
     </div>
   )
 }
