0f416b05097cfa38bef03fe04e79c39205f30a37
youba idjis <youba.etud@gmail.com>
2023-10-18 19:14:34 -0700

    herro updates

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -2,12 +2,12 @@ import React from 'react'
 import HeroSection from '../components/HeroSection/HeroSection'
 import ProjectCard from '../components/Projets/ProjectCard'
 import { ascards, clcards } from '../data/Association'
-
+import "../components/Projets/projets.css"
 const Clubs = () => {
   return (
     <>
       <HeroSection page="Clubs" cName="hero-mid" />
-      <div className='tradition'>
+      <div className='projets'>
         <h1>Clubs</h1>
         <div className='projectcard'>
           {clcards.map((card, index) =>
@@ -16,7 +16,7 @@ const Clubs = () => {
 
         </div>
       </div>
-      <div className='tradition'>
+      <div className='projets'>
         <h1>Associations</h1>
         <div className='projectcard'>
           {ascards.map((card, index) =>
