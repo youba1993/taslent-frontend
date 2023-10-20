44e3d547b1a68a28ecabfbf571f3c858cf5c38a3
youba idjis <youba.etud@gmail.com>
2023-10-19 18:43:58 -0700

    privacy

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -11,33 +11,33 @@ const PrivacyPolicy = () => {
       </p>
 
       <p>
-        <strong>1. Information We Collect</strong>
+        <strong>1. Information We Collect </strong>
         a. We may collect personal information that you voluntarily provide to us when you use our website. <br />
         b. This information may include your name, email address, and any other information you provide.
       </p>
 
       <p>
-        <strong>2. How We Use Your Information</strong>
+        <strong>2. How We Use Your Information </strong>
         a. We may use the information we collect for various purposes, including to provide and improve our services, to respond to your inquiries, and to send periodic emails.
       </p>
 
       <p>
-        <strong>3. Cookies</strong>
+        <strong>3. Cookies </strong>
         a. We use cookies to personalize content, to provide social media features, and to analyze our traffic. You consent to our cookies if you continue to use our website.
       </p>
 
       <p>
-        <strong>4. Third-Party Services</strong>
+        <strong>4. Third-Party Services </strong>
         a. We may use third-party services that collect, monitor, and analyze information to improve the functionality and security of our website.
       </p>
 
       <p>
-        <strong>5. Security</strong>
+        <strong>5. Security </strong>
         a. The security of your personal information is important to us, but remember that no method of transmission over the internet, or method of electronic storage is 100% secure.
       </p>
 
       <p>
-        <strong>6. Changes to This Privacy Policy</strong>
+        <strong>6. Changes to This Privacy Policy </strong>
         a. We reserve the right to update or change our Privacy Policy at any time. You are advised to review this Privacy Policy periodically for any changes.
       </p>
 
--- src/pages/TermsOfServices.js
+++ src/pages/TermsOfServices.js
@@ -2,48 +2,47 @@ import React from 'react';
 
 const TermsOfService = () => {
   return (
-    <div className="terms-container">
-          <a href='/'>retourn to taslent.org</a>
+    <div className="privacy-container">
       <h2>Terms of Service for taslent.org</h2>
 
       <p>
-        <strong>1. Acceptance of Terms</strong>
+        <strong>1. Acceptance of Terms </strong>
         By accessing or using the taslent.org website, you agree to comply with and be bound by the following Terms of Service.
         If you do not agree to these terms, please do not use the website.
       </p>
 
       <p>
-        <strong>2. Use of the Website</strong>
+        <strong>2. Use of the Website </strong>
         a. You must be at least 13 years old to use this website. <br />
         b. You are responsible for maintaining the confidentiality of your account and password. <br />
         c. You agree to provide accurate, current, and complete information during the registration process.
       </p>
 
       <p>
-        <strong>3. User Conduct</strong>
+        <strong>3. User Conduct </strong>
         a. You agree not to use the website for any unlawful purpose or to engage in any activity that would violate the rights of others. <br />
         b. You will not attempt to gain unauthorized access to any portion or feature of the website.
       </p>
 
       <p>
-        <strong>4. Intellectual Property</strong>
+        <strong>4. Intellectual Property </strong>
         a. All content on taslent.org, including text, graphics, logos, images, and software, is the property of taslent.org and is protected by copyright laws. <br />
         b. You may not reproduce, distribute, or create derivative works from any content without explicit permission.
       </p>
 
       <p>
-        <strong>5. Privacy Policy</strong>
+        <strong>5. Privacy Policy </strong>
         Your use of taslent.org is also governed by our Privacy Policy, which can be found <a href="/privacy">here</a>.
       </p>
 
       <p>
-        <strong>6. Limitation of Liability</strong>
+        <strong>6. Limitation of Liability </strong>
         a. taslent.org is provided on an "as-is" and "as-available" basis. We do not guarantee uninterrupted, secure, or error-free operation of the website. <br />
         b. taslent.org is not liable for any direct, indirect, incidental, consequential, or punitive damages arising out of your use of or inability to use the website.
       </p>
 
       <p>
-        <strong>7. Changes to Terms of Service</strong>
+        <strong>7. Changes to Terms of Service </strong>
         a. taslent.org reserves the right to update or change these Terms of Service at any time without notice. <br />
         b. Continued use of the website after any such changes shall constitute your consent to such changes.
       </p>
